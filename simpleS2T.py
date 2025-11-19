ea433e7ebbfd4c55a9c0040ee3c4a1725f3c53c0
anabah122 <66147287+anabah122@users.noreply.github.com>
2025-11-19 04:29:04 +0300

    Update simpleS2T.py

--- simpleS2T.py
+++ simpleS2T.py
@@ -5,17 +5,9 @@ import os
 import sys
 
 
-# Укажи ПОЛНЫЙ путь к распакованной папке модели
+
 MODEL_PATH = r"C:\Users\pcm\Downloads\SMWEB22\vosk-model-small-ru-0.22\\"
 
-# Проверка: существует ли папка и есть ли в ней model.conf
-if not os.path.isdir(MODEL_PATH):
-    print("Папка модели не найдена:", MODEL_PATH)
-    sys.exit(1)
-
-if not os.path.isfile(os.path.join(MODEL_PATH, "model.conf")):
-    print("Файл model.conf отсутствует. Убедись, что модель распакована полностью.")
-    sys.exit(1)
 
 model = Model(MODEL_PATH)
 
@@ -45,4 +37,5 @@ while True:
     else:
         partial = json.loads(rec.PartialResult())
         if partial.get("partial"):
+
             print( partial["partial"], end="\r", flush=True)
